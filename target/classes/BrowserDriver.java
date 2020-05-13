0d272f0143c5dc0b0c53aa30701decd352293068
JshajahanGoEasy <jamalmohamed.shajahan@sword-in.com>
2020-05-13 17:42:46 +0530

    Commit3
    Commit3

--- target/classes/BrowserDriver.java
+++ target/classes/BrowserDriver.java
@@ -12,41 +12,5 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class BrowserDriver {
-	
-	public static WebDriver driver;
-	public Properties prop;
-	
-	public WebDriver browser() throws IOException
-	{
-		prop = new Properties();
-		FileInputStream fy =new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\resources\\objects.properties");
-		prop.load(fy);
-		String browserName =prop.getProperty("browser");
-		
-			
-			
-			//Create a instance of ChromeOptions class
-			ChromeOptions options = new ChromeOptions();
-
-			//Add chrome switch to disable notification - "**--disable-notifications**"
-			options.addArguments("--disable-notifications");
-			
-
-			//Set path for driver exe 
-			System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\main\\resources\\Browser Drivers\\chromedriver.exe");
-
-			//Pass ChromeOptions instance to ChromeDriver Constructor
-			 driver =new ChromeDriver(options);
-			 driver.manage().window().maximize();
-		
-	
-		return driver;
-		
-	
-	
-	
-}
-	
-
 }
 
